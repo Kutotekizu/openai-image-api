d4f000994f7e33cb6fd58bd6dba9db6aa3567e27
Kutotekizu <122458482+Kutotekizu@users.noreply.github.com>
2025-06-19 17:24:44 +0200

    Update generate.js

--- api/generate.js
+++ api/generate.js
@@ -51,7 +51,7 @@ export default async function handler(req, res) {
         'Content-Type': 'application/json'
       },
       body: JSON.stringify({
-        model: 'dall-e-3',
+        model: 'dall-e-2',
         prompt,
         n: 1,
         size: "1024x1024"
