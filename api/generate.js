7cfbc86d15b70b09be73ae16e8c73b02fae8264a
Kutotekizu <122458482+Kutotekizu@users.noreply.github.com>
2025-06-19 17:19:42 +0200

    Update generate.js

--- api/generate.js
+++ api/generate.js
@@ -3,6 +3,8 @@ export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
+  console.log('Request body (raw):', req.body);
+
   if (req.method === 'OPTIONS') {
     res.status(200).end();
     return;
